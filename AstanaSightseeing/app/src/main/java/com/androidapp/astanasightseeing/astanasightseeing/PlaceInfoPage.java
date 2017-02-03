249cfafda75f166b661badbcc4b61cbd387af627
Bekmurat Spayev <spayev@gmail.com>
2017-02-03 12:26:04 +0500

    ...
    ...

--- AstanaSightseeing/app/src/main/java/com/androidapp/astanasightseeing/astanasightseeing/PlaceInfoPage.java
+++ AstanaSightseeing/app/src/main/java/com/androidapp/astanasightseeing/astanasightseeing/PlaceInfoPage.java
@@ -1,43 +1,58 @@
 package com.androidapp.astanasightseeing.astanasightseeing;
 
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.w3c.dom.Text;
 
 import java.util.HashMap;
 
-public class PlaceInfoPage extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
+public class PlaceInfoPage extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener, OnMapReadyCallback {
 
     private SliderLayout mDemoSlider;
     TextView tvPlaceName;
+    MapView mapView;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_info_page);
-        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
+        mDemoSlider = (SliderLayout) findViewById(R.id.slider);
         tvPlaceName = (TextView) findViewById(R.id.tvPlaceName);
 
         String value = getIntent().getExtras().getString("chosenPlaceName");
         System.out.println("Name : " + value);
-
         tvPlaceName.setText(value);
 
-        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
-        file_maps.put("Hannibal",R.drawable.baiterek_pic);
-        file_maps.put("Big Bang Theory",R.drawable.hazret_sultan_pic);
-        file_maps.put("House of Cards",R.drawable.khan_shatyr_pic);
+        HashMap<String, Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("Hannibal", R.drawable.baiterek_pic);
+        file_maps.put("Big Bang Theory", R.drawable.hazret_sultan_pic);
+        file_maps.put("House of Cards", R.drawable.khan_shatyr_pic);
         file_maps.put("Game of Thrones", R.drawable.nur_astana_pic);
-/*
-        for(String name : file_maps.keySet()){
+
+        for (String name : file_maps.keySet()) {
             TextSliderView textSliderView = new TextSliderView(this);
             // initialize a SliderLayout
             textSliderView
@@ -49,7 +64,7 @@ public class PlaceInfoPage extends AppCompatActivity implements BaseSliderView.O
             //add your extra information
             textSliderView.bundle(new Bundle());
             textSliderView.getBundle()
-                    .putString("extra",name);
+                    .putString("extra", name);
 
             mDemoSlider.addSlider(textSliderView);
         }
@@ -57,9 +72,17 @@ public class PlaceInfoPage extends AppCompatActivity implements BaseSliderView.O
         mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
         mDemoSlider.setCustomAnimation(new DescriptionAnimation());
         mDemoSlider.setDuration(4000);
-        mDemoSlider.addOnPageChangeListener(this);*/
+        mDemoSlider.addOnPageChangeListener(this);
 
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mapView.getMapAsync(PlaceInfoPage.this);
+        // Log.i(TAG, "View created");
     }
+
 
     @Override
     public void onSliderClick(BaseSliderView slider) {
@@ -80,4 +103,25 @@ public class PlaceInfoPage extends AppCompatActivity implements BaseSliderView.O
     public void onPageScrollStateChanged(int state) {
 
     }
-}
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+//DO WHATEVER YOU WANT WITH GOOGLEMAP
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        map.setTrafficEnabled(true);
+        map.setIndoorEnabled(true);
+        map.setBuildingsEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+    }
+    }
